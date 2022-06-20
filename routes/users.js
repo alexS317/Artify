daae37a25897c272a49326e79e64124b7364d850
AlexandraF <cc211020@fhstp.ac.at>
2022-06-20 09:08:42 +0200

    commit day 6_1

--- routes/users.js
+++ routes/users.js
@@ -14,40 +14,42 @@ router.get('/:id/edit', userController.editUser);
 router.post('/:id', userController.updateUser);
 
 router.get('/:id/gallery', (req, res) => res.render('userGallery'));
+router.get('/:id/artwork', (req, res) => res.render('artwork'));
+router.get('/:id/upload', (req, res) => res.render('uploadArtwork'));
 
 
-router.route('/:id/artwork')
-    .get((req, res) => {
-        const filename = req.params.profilepic + '.png';
-        const options = {
-            root: path.join(__dirname, '../uploads')
-        }
-        res.sendFile(filename, options);
-    })
-    .post((req, res) => {
-        try {
-            if(!req.files) {
-                res.send({
-                    status: false,
-                    message: 'No file has been uploaded.'
-                });
-            } else {
-                let profilepic = req.files.profilepic;
-                let filename = './uploads/' + uuid() + 'png';
-                profilepic.mv(filename);
-                console.log('Saved picture to: ' + filename);
-                res.send({
-                    status: true,
-                    message: 'File is uploaded.',
-                    data: {
-                        name: profilepic.name
-                    }
-                });
-            }
-        } catch(err) {
-            res.sendStatus(500).send(err);
-        }
-    })
+// router.route('/:id/artwork')
+//     .get((req, res) => {
+//         const filename = req.params.profilepic + '.png';
+//         const options = {
+//             root: path.join(__dirname, '../uploads')
+//         }
+//         res.sendFile(filename, options);
+//     })
+//     .post((req, res) => {
+//         try {
+//             if(!req.files) {
+//                 res.send({
+//                     status: false,
+//                     message: 'No file has been uploaded.'
+//                 });
+//             } else {
+//                 let profilepic = req.files.profilepic;
+//                 let filename = './uploads/' + uuid() + 'png';
+//                 profilepic.mv(filename);
+//                 console.log('Saved picture to: ' + filename);
+//                 res.send({
+//                     status: true,
+//                     message: 'File is uploaded.',
+//                     data: {
+//                         name: profilepic.name
+//                     }
+//                 });
+//             }
+//         } catch(err) {
+//             res.sendStatus(500).send(err);
+//         }
+//     })
 
 
 module.exports = router;    // Export file as a router to be used in other files
