227a1582dada632df432c3a6b577a0b9825be74b
AlexandraF <cc211020@fhstp.ac.at>
2022-06-26 03:56:42 +0200

    commit

---
+++ secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+    dbPassword: "m?SVbJNQ5G[C",
+    auth_token_secret: "46874337d030ec01b6c06d7d75c992441b60ff6af15c0e24e9291012a0cd8a20ec63caa85716e25bd0ad9f718ace852ec21397c58d416ee7e6a9a4f733911bf5"
+};
